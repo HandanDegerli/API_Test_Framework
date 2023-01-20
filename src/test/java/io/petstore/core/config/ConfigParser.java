194be2f9ec5aba6efb6b937786aad1853a6f0c06
HandanDegerli <dgrl.handan@gmail.com>
2023-01-20 19:58:14 +0100

    Fix

--- src/test/java/io/petstore/core/config/ConfigParser.java
+++ src/test/java/io/petstore/core/config/ConfigParser.java
@@ -1,5 +1,6 @@
 package io.petstore.core.config;
 
+import org.apache.log4j.Logger;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileReader;
@@ -7,10 +8,9 @@ import java.io.Reader;
 import java.util.Map;
 
 public class ConfigParser {
-
     private static final String CONFIG_FILE_NAME = "./configuration.yml";
-
     public static String getValue(String key) {
+        Logger logger = Logger.getLogger(ConfigParser.class);
         Yaml yaml = new Yaml();
         try{
             Reader yamlFile = new FileReader(CONFIG_FILE_NAME);
@@ -19,6 +19,7 @@ public class ConfigParser {
             return (String) yamlMaps.get(key);
         }catch (Exception e) {
             System.out.println(e);
+            logger.error("Configuration File could not mapped");
         }
         return null;
     }
